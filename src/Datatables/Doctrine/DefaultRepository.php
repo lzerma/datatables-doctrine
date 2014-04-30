69e6c652e476d01994e4cc0784df6bc7df0c1d0b
Rafael Mitsuo Moriya <rm.moriya@gmail.com>
2014-04-30 16:39:05 -0300

    exception caso nao esteja setado os parametros principais

--- src/Datatables/Doctrine/DefaultRepository.php
+++ src/Datatables/Doctrine/DefaultRepository.php
@@ -22,95 +22,105 @@ class DefaultRepository extends EntityRepository {
     /**
      *
      * @var array 
+     * array[]['campo']
+     * array[]['type']
      */
-    private $aColumns;
+    private $aColumns = null;
 
     /**
      *
      * @var array 
+     * array[]['campo']
+     * array[]['html']
      */
-    private $aColumnsArray;
+    private $aColumnsArray = null;
 
     public function getDatatables(QueryBuilder $qb, array $params) {
 
-        if ($_GET['sSearch'] != "") {
-            for ($i = 0; $i < count($this->getAColumns()); $i++) {
-                if ($this->getAColumns()[$i]['type'] == "string") {
-                    $qb->orWhere("LOWER(remove_accents(". $this->getAColumns()[$i]['campo'] . ")) LIKE LOWER(remove_accents(:busca" . $i . "))")
-                    ->setParameter("busca" . $i , $params['sSearch']);
-                } else {
-                    if (is_int($params['sSearch'])) {
-                        $qb->orWhere($this->getAColumns()[$i]['campo'] . " = '" . $params['sSearch'] . "'");
+        if($this->getAColumns() != '' && $this->getAColumnsArray() != ''){
+            if ($_GET['sSearch'] != "") {
+                for ($i = 0; $i < count($this->getAColumns()); $i++) {
+                    if ($this->getAColumns()[$i]['type'] == "string") {
+                        $qb->orWhere("LOWER(remove_accents(". $this->getAColumns()[$i]['campo'] . ")) LIKE LOWER(remove_accents(:busca" . $i . "))")
+                        ->setParameter("busca" . $i , $params['sSearch']);
+                    } else {
+                        if (is_int($params['sSearch'])) {
+                            $qb->orWhere($this->getAColumns()[$i]['campo'] . " = '" . $params['sSearch'] . "'");
+                        }
                     }
                 }
             }
-        }
 
         // Select total
-        $stmt = $this->getEntityManager()
-        ->getConnection()
-        ->prepare("SELECT count(a) FROM (" . $this->get_raw_sql($qb) . ") AS a");
+            $stmt = $this->getEntityManager()
+            ->getConnection()
+            ->prepare("SELECT count(a) FROM (" . $this->get_raw_sql($qb) . ") AS a");
 
-        $stmt->execute();
+            $stmt->execute();
 
 
-        if (isset($params['iSortCol_0'])) {
-            $primeiro = true;
-            for ($i = 0; $i < intval($params['iSortingCols']); $i++) {
-                if ($params['bSortable_' . intval($params['iSortCol_' . $i])] == "true") {
-                    if ($primeiro) {
-                        $primeiro = false;
-                        $qb->orderBy($this->getAColumns()[intval($params['iSortCol_' . $i])]['campo'], $params['sSortDir_' . $i]);
-                    } else {
-                        $qb->add($this->getAColumns()[intval($params['iSortCol_' . $i])]['campo'], $params['sSortDir_' . $i]);
+            if (isset($params['iSortCol_0'])) {
+                $primeiro = true;
+                for ($i = 0; $i < intval($params['iSortingCols']); $i++) {
+                    if ($params['bSortable_' . intval($params['iSortCol_' . $i])] == "true") {
+                        if ($primeiro) {
+                            $primeiro = false;
+                            $qb->orderBy($this->getAColumns()[intval($params['iSortCol_' . $i])]['campo'], $params['sSortDir_' . $i]);
+                        } else {
+                            $qb->add($this->getAColumns()[intval($params['iSortCol_' . $i])]['campo'], $params['sSortDir_' . $i]);
+                        }
                     }
                 }
             }
-        }
 
-        for ($i = 0; $i < count($this->getAColumns()); $i++) {
-            if ($_GET['bSearchable_' . $i] == "true" && $params['sSearch_' . $i] != '') {
-                if ($this->getAColumns()[$i]['type'] == "string") {
-                    $qb->orWhere("LOWER(remove_accents(". $this->getAColumns()[$i]['campo'] . ")) LIKE LOWER(remove_accents(:busca" . $i . "))")
-                    ->setParameter("busca" . $i , $params['sSearch_' . $i]);
-                } else {
-                    if (is_int($params['sSearch'])) {
-                        $qb->andWhere($this->getAColumns()[$i]['campo'] . " = '" . $params['sSearch_' . $i] . "'");
-                    }
-                }
-            }
-        }
-        
-
-        $qb->setMaxResults($params['iDisplayLength']);
-        $qb->setFirstResult($params['iDisplayStart']);
-
-        $rows = array();
-        foreach ($qb->getQuery()->getResult() as $key => $value) {
-            $aRow = $value->toArray();
-
-            $row = array();
             for ($i = 0; $i < count($this->getAColumns()); $i++) {
-                if (isset($this->getAColumnsArray()[$i]['html']) && trim($this->getAColumnsArray()[$i]['campo']) != '') {
-                    $row[] = str_replace("{campo}", $aRow[$this->getAColumnsArray()[$i]['campo']], $this->getAColumnsArray()[$i]['html']);
-                } else {
-                    $row[] = $aRow[$this->getAColumnsArray()[$i]['campo']];
+                if ($_GET['bSearchable_' . $i] == "true" && $params['sSearch_' . $i] != '') {
+                    if ($this->getAColumns()[$i]['type'] == "string") {
+                        $qb->orWhere("LOWER(remove_accents(". $this->getAColumns()[$i]['campo'] . ")) LIKE LOWER(remove_accents(:busca" . $i . "))")
+                        ->setParameter("busca" . $i , $params['sSearch_' . $i]);
+                    } else {
+                        if (is_int($params['sSearch'])) {
+                            $qb->andWhere($this->getAColumns()[$i]['campo'] . " = '" . $params['sSearch_' . $i] . "'");
+                        }
+                    }
                 }
             }
-            $rows[] = $row;
+
+
+            $qb->setMaxResults($params['iDisplayLength']);
+            $qb->setFirstResult($params['iDisplayStart']);
+
+            $rows = array();
+            foreach ($qb->getQuery()->getResult() as $key => $value) {
+                $aRow = $value->toArray();
+
+                $row = array();
+                for ($i = 0; $i < count($this->getAColumns()); $i++) {
+                    if (isset($this->getAColumnsArray()[$i]['html']) && trim($this->getAColumnsArray()[$i]['campo']) != '') {
+                        $row[] = str_replace("{campo}", $aRow[$this->getAColumnsArray()[$i]['campo']], $this->getAColumnsArray()[$i]['html']);
+                    } else {
+                        $row[] = $aRow[$this->getAColumnsArray()[$i]['campo']];
+                    }
+                }
+                $rows[] = $row;
+            }
+
+
+            $total = $stmt->fetchAll()[0]['count'];
+            $output = array(
+                "sEcho" => $params['sEcho'],
+                "iTotalRecords" => $total,
+                "iTotalDisplayRecords" => $total,
+                "iDisplayLength" => $params['iDisplayLength'],
+                "aaData" => $rows
+                );
+
+            return $output;
+        }else{
+            throw new \Exception("The value(getAColumns, getAColumnsArray) is required", 1);
         }
 
-
-        $total = $stmt->fetchAll()[0]['count'];
-        $output = array(
-            "sEcho" => $params['sEcho'],
-            "iTotalRecords" => $total,
-            "iTotalDisplayRecords" => $total,
-            "iDisplayLength" => $params['iDisplayLength'],
-            "aaData" => $rows
-            );
-
-        return $output;
+        
     }
 
     public function getIDisplayStart() {
